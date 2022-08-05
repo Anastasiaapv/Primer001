141a4f14a1aa84dc027b9c344cceaa00adc1ffdb
Anastasia <anastasiaapv@gmail.com>
2022-08-05 18:02:25 +0300

    Primer005_user

---
+++ Primer005_user/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "настя")
+{
+    Console.WriteLine("Ура, это же Настя!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
