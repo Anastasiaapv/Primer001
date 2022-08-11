5750600737839c6fd544e05a099ffaeb10a804f9
Anastasia <anastasiaapv@gmail.com>
2022-08-11 14:13:51 +0300

    Primer008_IntroMetod2

--- Primer008_IntroMetod/Program.cs
+++ Primer008_IntroMetod/Program.cs
@@ -1,12 +1,4 @@
-﻿int Max( int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2> result) result = arg2;
-    if (arg3> result) result = arg3;
-    return result;
-}
-
-
+﻿int Max( int arg1, int arg2, int arg3);
 int a1=1;
 int b1=7;
 int c1=78;
@@ -17,13 +9,44 @@ int a3=16;
 int b3=54;
 int c3=152;
 
+{
+    int result = arg1;
+    if (arg2> result) result = arg2;
+    if (arg3> result) result = arg3;
+    return result;
+}            //  0  1  2  3  4  5 6  7 8
+// int[] array = {19,62,93,94,50,66,7,83,9};
+// // array[0]=20;
+// int result = Max(Max(array[0],array[2],array[3]),
+//           Max(array[4],array[5],array[6]),
+//           Max(array[7],array[8],array[9]));
+
+// Console.WriteLine(result);
+
+
+
+// int max = Max(Max(a1,b1,c1),
+//           Max(a2,b2,c2),
+//           Max(a3,b3,c3));
+// Console.WriteLine(max);
+
+
+
+
+// int a1=1;
+// int b1=7;
+// int c1=78;
+// int a2=5;
+// int b2=12;
+// int c2=48;
+// int a3=16;
+// int b3=54;
+// int c3=152;
+
 // int max1 = Max(a1,b1,c1);
 // int max2 = Max(a2,b2,c2);
 // int max3 = Max(a3,b3,c3);
 // int max = Max(max1,max2,max3);
-int max = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
-Console.WriteLine(max);
-
 
 // if (b1 > max ) max =b1;
 // if (c1 > max ) max =c1;
