c9f707213dd54f2a0cd5f03a72882ad23b56d98f
Anastasia <anastasiaapv@gmail.com>
2022-08-12 18:45:36 +0300

    Primer010_MetodArray

---
+++ Primer010_MetodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1,82,34,4,75,68,577,68};
+int n = array.Length;
+int find=68;
+int index = 0;
+while (index < n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
